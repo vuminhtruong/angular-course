f35459c20a88e6beb4d92e73ebc136b0b49805a9
vuminhtruong <vuminhtruongvmu18@gmail.com>
2024-03-20 08:53:39 +0700

    generate course-card component

--- angular-course-1-components/src/app/app.module.ts
+++ angular-course-1-components/src/app/app.module.ts
@@ -3,15 +3,17 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {CourseCardComponent} from './course-card/course-card.component';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
-  imports: [
-    BrowserModule,
-    BrowserAnimationsModule
-  ],
+    imports: [
+        BrowserModule,
+        BrowserAnimationsModule,
+        CourseCardComponent
+    ],
   providers: [],
   bootstrap: [AppComponent]
 })
