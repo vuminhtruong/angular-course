52f9dcfd43fe2c5d39e85c12902a056f43386a3b
vuminhtruong <vuminhtruongvmu18@gmail.com>
2024-03-21 11:41:36 +0700

    async pipe&observable

--- angular-course-3-services/src/app/app.component.ts
+++ angular-course-3-services/src/app/app.component.ts
@@ -1,9 +1,11 @@
+// @ts-ignore
 import {AfterViewInit, Component, ElementRef, OnInit, QueryList, ViewChild, ViewChildren} from '@angular/core';
 import {COURSES} from '../db-data';
 import {Course} from './model/course';
 import {CourseCardComponent} from './course-card/course-card.component';
 import {HighlightedDirective} from './directives/highlighted.directive';
 import {Observable} from 'rxjs';
+import {HttpClient, HttpParams} from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
@@ -11,17 +13,18 @@ import {Observable} from 'rxjs';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent implements OnInit {
+  courses$: Observable<Course[]>;
 
-
-  courses = COURSES;
-
-  constructor() {
+  constructor(private http: HttpClient) {
 
   }
 
   ngOnInit() {
+    const params = new HttpParams()
+      .set('page', '1')
+      .set('size', '10');
+
+    this.courses$ = this.http.get<Course[]>('api/courses', {params});
   }
-
-
 
 }
